7d34fbd99c1c2735b41bcb1c1337f3808408b94f
Ashish Singh <ashishsingh.1899@gmail.com>
2022-08-24 23:16:05 +0530

    minor config

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+require("dotenv").config();
 
 import postRoutes from "./routes/posts.js";
 
@@ -13,8 +14,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://ashishsng:ashish8423@cluster0.igdovfh.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = `mongodb+srv://ashishsng:${process.env.MONGO_PASSWORD}@cluster0.igdovfh.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
