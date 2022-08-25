2860308de926b950ccbe7b6a19f746ee3e1e1a65
Ashish Singh <ashishsingh.1899@gmail.com>
2022-08-25 11:07:12 +0530

    config conflict

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 import postRoutes from "./routes/posts.js";
 
@@ -14,14 +15,17 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://ashishsng:${process.env.MONGO_PASSWORD}@cluster0.igdovfh.mongodb.net/?retryWrites=true&w=majority`;
+app.get("/", (res, req) => {
+  res.send("Hello to Memories API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 
   mongoose
-    .connect(CONNECTION_URL)
+    .connect(process.env.CONNECTION_URL)
     .then(() => {
       console.log("MongoDB Connected!");
     })
